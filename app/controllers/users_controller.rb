9e9ed0ba43c0c009741ac8fcf4c7e09ac6be2806
Rich Kaht <adamselene1776@protonmail.com>
2022-12-13 11:18:12 -0700

    Add session for login_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,8 @@ class UsersController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user)
     else
       redirect_to '/login'
